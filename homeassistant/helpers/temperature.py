7b8096e3c73ca9096d1b1e29dd94f6e6f2dabcaa
Nathanserra <nathanserra99@gmail.com>
2023-02-03 23:13:03 -0300

    refatorando e criando função das temperaturas

--- homeassistant/helpers/temperature.py
+++ homeassistant/helpers/temperature.py
@@ -1,40 +1,56 @@
 """Temperature helpers for Home Assistant."""
-from __future__ import annotations
+from future import annotations
 
 from numbers import Number
 
-from homeassistant.const import PRECISION_HALVES, PRECISION_TENTHS
+from homeassistant.const import PRECISION_HALVES, PRECISION_TENTHS, PRECISION_WHOLE, TEMP_CELSIUS, TEMP_FAHRENHEIT, TEMP_KELVIN
 from homeassistant.core import HomeAssistant
 from homeassistant.util.unit_conversion import TemperatureConverter
 
 
 def display_temp(
-    hass: HomeAssistant, temperature: float | None, unit: str, precision: float
+    hass: HomeAssistant, temperature: float | None, unit: str | None, precision: float
 ) -> float | None:
     """Convert temperature into preferred units/precision for display."""
     temperature_unit = unit
     ha_unit = hass.config.units.temperature_unit
-
+    
     if temperature is None:
         return temperature
-
+    
     # If the temperature is not a number this can cause issues
     # with Polymer components, so bail early there.
     if not isinstance(temperature, Number):
         raise TypeError(f"Temperature is not a number: {temperature}")
+        
+    temperature = TemperatureConverter.convert(temperature, ha_unit, temperature_unit)
 
-    if temperature_unit != ha_unit:
-        temperature = TemperatureConverter.convert(
-            temperature, temperature_unit, ha_unit
-        )
+    return round_temperature(temperature, precision)
 
-    # Round in the units appropriate
+
+def round_temperature(
+    temperature: float, precision: float
+) -> float:
+    """Round in the units appropriate for the precision."""
+
+    # convert to halves precision
     if precision == PRECISION_HALVES:
-        temperature = round(temperature * 2) / 2.0
+        return round(temperature * 2) / 2
+    #convert to tenths precision
     elif precision == PRECISION_TENTHS:
-        temperature = round(temperature, 1)
-    # Integer as a fall back (PRECISION_WHOLE)
-    else:
-        temperature = round(temperature)
+        return round(temperature, 1)
+    
+    # convert to Whole number
+    return round(temperature)
 
-    return temperature
+
+#shows temperature in all different units of measurement
+def differents_units_of_measurement(
+    hass: HomeAssistant, temperature: float | None, precision: str
+    )-> dict[str, float | None]:
+    """Get temperature in all different units of measurement."""
+    return {
+        TEMP_CELSIUS: display_temp(hass, temperature, TEMP_CELSIUS, precision),
+        TEMP_FAHRENHEIT: display_temp(hass, temperature, TEMP_FAHRENHEIT, precision),
+        TEMP_KELVIN: display_temp(hass, temperature, TEMP_KELVIN, precision),
+    }
--- tests/helpers/test_temperature.py
+++ tests/helpers/test_temperature.py
@@ -7,31 +7,54 @@ from homeassistant.const import (
     PRECISION_WHOLE,
     TEMP_CELSIUS,
     TEMP_FAHRENHEIT,
+    TEMP_KELVIN,
 )
-from homeassistant.helpers.temperature import display_temp
+from homeassistant.helpers.temperature import display_temp, differents_units_of_measurement, round_temperature
 
 TEMP = 24.636626
 
+def test_round_temperature(hass):
+    """Test round_temperature."""
+    assert round_temperature(TEMP, PRECISION_WHOLE) == 25
+    assert round_temperature(TEMP, PRECISION_TENTHS) == 24.6
+    assert round_temperature(TEMP, PRECISION_HALVES) == 24.5
 
-def test_temperature_not_a_number(hass):
-    """Test that temperature is a number."""
-    temp = "Temperature"
-    with pytest.raises(Exception) as exception:
-        display_temp(hass, temp, TEMP_CELSIUS, PRECISION_HALVES)
+def test_display_temp(hass):
+    """Test display_temp."""
+    assert display_temp(hass, TEMP, TEMP_CELSIUS, PRECISION_WHOLE) == 25
+    assert display_temp(hass, TEMP, TEMP_FAHRENHEIT, PRECISION_WHOLE) == 76
+    assert display_temp(hass, TEMP, TEMP_KELVIN, PRECISION_WHOLE) == 298
 
-    assert f"Temperature is not a number: {temp}" in str(exception.value)
-
-
-def test_celsius_halves(hass):
-    """Test temperature to celsius rounding to halves."""
-    assert display_temp(hass, TEMP, TEMP_CELSIUS, PRECISION_HALVES) == 24.5
-
-
-def test_celsius_tenths(hass):
-    """Test temperature to celsius rounding to tenths."""
     assert display_temp(hass, TEMP, TEMP_CELSIUS, PRECISION_TENTHS) == 24.6
+    assert display_temp(hass, TEMP, TEMP_FAHRENHEIT, PRECISION_TENTHS) == 76.3
+    assert display_temp(hass, TEMP, TEMP_KELVIN, PRECISION_TENTHS) == 297.8
 
+    assert display_temp(hass, TEMP, TEMP_CELSIUS, PRECISION_HALVES) == 24.5
+    assert display_temp(hass, TEMP, TEMP_FAHRENHEIT, PRECISION_HALVES) == 76.5
+    assert display_temp(hass, TEMP, TEMP_KELVIN, PRECISION_HALVES) == 298.0
 
-def test_fahrenheit_wholes(hass):
-    """Test temperature to fahrenheit rounding to wholes."""
-    assert display_temp(hass, TEMP, TEMP_FAHRENHEIT, PRECISION_WHOLE) == -4
+def test_display_temp_no_temperature(hass):
+    """Test display_temp with no temperature."""
+    assert display_temp(hass, None, TEMP_CELSIUS, PRECISION_WHOLE) is None
+
+def test_display_temp_invalid_temperature(hass):
+    """Test display_temp with invalid temperature."""
+    with pytest.raises(TypeError):
+        display_temp(hass, "not a number", TEMP_CELSIUS, PRECISION_WHOLE)
+
+def test_differents_units_of_measurement(hass):
+    assert differents_units_of_measurement(hass, TEMP, PRECISION_WHOLE) == { 
+        TEMP_CELSIUS: 25, 
+        TEMP_FAHRENHEIT: 76, 
+        TEMP_KELVIN:  298
+        }
+    assert differents_units_of_measurement(hass, TEMP, PRECISION_TENTHS) == { 
+        TEMP_CELSIUS: 24.6, 
+        TEMP_FAHRENHEIT: 76.3, 
+        TEMP_KELVIN: 297.8
+        }
+    assert differents_units_of_measurement(hass, TEMP, PRECISION_HALVES) == { 
+        TEMP_CELSIUS: 24.5,
+        TEMP_FAHRENHEIT: 76.5, 
+        TEMP_KELVIN: 298.0
+        }
